8d24d82c1df0795cf40b588cf66d07caaa6de923
pedro.novaes <pedro.barros@sptech.school>
2025-04-22 08:23:42 -0300

    Subindo Controller.

---
+++ atividade01/src/main/java/com/example/atividade01/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.example.atividade01.repository;
+
+import com.example.atividade01.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
